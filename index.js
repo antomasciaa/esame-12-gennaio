05f51b2b6c7c0b40d3f88b00dbacac1feb367967
antoniomasciaa <antonio.mascia@studenti.unitn.it>
2018-08-10 16:39:01 +0200

    index complete

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var getArea = require ('./area.js');
+var port = process.env.PORT || 8080;
+var query = require('querystring');
 
 var app = express();
 
 
 app.get('./area', function(req, res){
-	res.write("Esame 12 gennaio");
-	res.end();
+	var lato1 = req.query.lato1;
+	var lato2 = req.query.lato2;
+	
+	res.json({
+		area: getArea([parseInt(lato1), parseInt(lato2)])
+    });
+
 }); 
+
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
