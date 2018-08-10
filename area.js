360998852060cd08b24a9f81f01ea2f5c1805329
antoniomasciaa <antonio.mascia@studenti.unitn.it>
2018-08-10 16:30:22 +0200

    function area complete

---
+++ area.js
@@ -0,0 +1,23 @@
+var getArea = function area(lati){
+	if(lati == null){
+		console.log("Error: null input");
+		return -1;
+	} if(!(lati instanceof Array)){
+		console.log("Error: input is not an array");
+		return -1;
+	} if(lati.lenght != 2){
+		console.log("Error: input array has size " + lat.length + " instead of 2");
+		return -1;
+	} if(!Number.isInteger(lat[0]) || !Number.isInteger(lat[1])){
+		console.log("Error: input value needs to be integers");
+		return -1;
+	} if(lat[0] >= 0 && lat[1] >= 0){
+    	var prodotto = lat[0]*lat[1];
+    	return prodotto;
+  	} else {
+    	console.log("Error: input value needs to be positive");
+    	return -1;
+  	}
+}
+
+module.module.exports = getArea;
